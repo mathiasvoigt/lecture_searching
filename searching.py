aa325ac76d416b3f678833c870b91cce9f4bad79
mathiasvoigt <257067@vutbt.cz>
2024-04-08 12:24:55 +0200

    test

--- searching.py
+++ searching.py
@@ -2,6 +2,8 @@ import os
 
 # get current working directory path
 cwd_path = os.getcwd()
+import json
+
 
 
 def read_data(file_name, field):
@@ -13,9 +15,21 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open("sequential.json", "r") as f:
+        allowed_keys = json.load(f)["allowed_keys"]
+
+    if field not in allowed_keys:
+        return None
+
+    with open(file_path) as data_file:
+        data = json.load(data_file)
+
+    return data.get(field, None)
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print("Sequential_data", sequential_data)
+
 
 
 if __name__ == '__main__':
