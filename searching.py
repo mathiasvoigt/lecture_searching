dc56a119a0dc591ab444410d97a3a8b147c22554
mathiasvoigt <257067@vutbt.cz>
2024-04-08 12:49:47 +0200

    test2

--- searching.py
+++ searching.py
@@ -15,21 +15,37 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
-    with open("sequential.json", "r") as f:
-        allowed_keys = json.load(f)["allowed_keys"]
+    with open("sequential.json", "r") as data_file:
+        data = json.load(data_file)
 
-    if field not in allowed_keys:
-        return None
+    for key in data.keys():
+        if field == key:
+            sequential_data = data[field]
+            return sequential_data
+        else:
+            return None
 
     with open(file_path) as data_file:
         data = json.load(data_file)
 
     return data.get(field, None)
 
+def linear_search(sequence, number):
+    positions = []
+    count = 0
+    for i in range(len(sequence)):
+        if sequence[i] == number:
+            positions.append(i)
+            count = count + 1
+    return {"positions": positions, "count": count}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print("Sequential_data", sequential_data)
-
+    dictionary = linear_search(sequential_data, 0)
+    print(dictionary)
+    return
 
 
 if __name__ == '__main__':
